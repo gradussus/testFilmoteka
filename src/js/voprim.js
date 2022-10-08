b005ef6b09d327de0691d7e9ea4655f11800c37a
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-10-08 19:12:34 +0300

    Update voprim.js

--- src/js/voprim.js
+++ src/js/voprim.js
@@ -7,14 +7,24 @@ import { refs } from './refs';
 
 export default function renderFilmsMarkup(films) {
   films
-    .map(({ poster_path, title, id }) => {
+    .map(({ poster_path, title, release_date, genres, id }) => {
       // let filmGenres = genres.map(({ name }) => name).join(', ');
 
       return `<li class="films__item" data-id=${id}>
                 <div class="films__img">
                     <img src=https://image.tmdb.org/t/p/original${poster_path} alt="${title}" loading="lazy">
                 </div>
-                
+                <div class="films__description">
+                  <p class="films__title">${title}</p>
+                  <div class="films__meta">
+                    <span class="films__genres">${genres || 'Action'}</span>
+                    <span class="films__sep">|</span>
+                    <span class="films__data">${(release_date || '2022').slice(
+                      0,
+                      4
+                    )}</span>
+                  </div>
+                </div>
             </li>`;
     })
     .forEach(c => refs.gallery.insertAdjacentHTML('beforeend', c));
